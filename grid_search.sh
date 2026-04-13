f259dd5e5156b7a7a6c1a8cf3f1ea9b7d3193594
kevin <kehaoxu96@gmail.com>
2026-04-13 01:55:32 -0400

    update

--- grid_search.sh
+++ grid_search.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+# sbatch --export=ALL,GEN_RATIO="0.2",OUT_DIR="outputs/atlas1+uncond22" train.sbatch
+
 GEN_RATIOS=(
-  0.3
-  0.4
-  0.5
-  0.6
-  0.7
-  0.8
-  0.9
+  0.2
 )
 
 for gr in "${GEN_RATIOS[@]}"; do
