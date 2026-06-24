9a785bf7578280d7b2cf429f009bc5ab9e4c6c9b
KehaoXu <kehaoxu96@gmail.com>
2026-06-24 02:16:25 -0400

    update

--- USB/autoencoders/autoencoders/architectures/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .cnn import *
-from .lpips import *
-__all__ = ["Encoder", "VariationalEncoder", 
-           "Decoder", "LPIPS", "NLayerDiscriminator"]
-classes = __all__ 
--- USB/autoencoders/autoencoders/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from  .aekl import *
-from .aekl_ad import *
-from .vaebase import *
-from .cvaebase import *
-__all__ = [ "AutoencoderKL", "AutoencoderKLAD", "vaebase", "cvaebase"]
-classes = __all__ 
